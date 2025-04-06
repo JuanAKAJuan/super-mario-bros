669ca1293b3eccf2f11dfcab4e95f072845dd9c6
Juan Mireles <juanmireles@proton.me>
2025-04-05 21:37:05 -0400

    1. Finish animations and add some audio. 2. Have the camera follow Mario.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // <summary>
+    // Mario's Transform.
+    // </summary>
+    public Transform player;
+
+    // <summary>
+    // GameObject that indicates the end of the map.
+    // </summary>
+    public Transform endLimit;
+
+    // <summary>
+    // Initial x-offset between the camera and Mario.
+    // </summary>
+    private float offset;
+
+    // <summary>
+    // Smallest x-coordinate of the camera.
+    // </summary>
+    private float startX;
+
+    // <summary>
+    // Largest x-coordinate of the camera.
+    // </summary>
+    private float endX;
+
+    private float viewportHalfWidth;
+
+    private void Start()
+    {
+        // Get the coordinate of the bottom left of the viewport.
+        // z doesn't matter since the camera is orthographic.
+        Vector3 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        viewportHalfWidth = Mathf.Abs(bottomLeft.x - this.transform.position.x);
+        offset = this.transform.position.x - player.position.x;
+        startX = this.transform.position.x;
+        endX = endLimit.transform.position.x - viewportHalfWidth;
+    }
+
+    private void Update()
+    {
+        float desiredX = player.position.x + offset;
+        if (desiredX > startX && desiredX < endX)
+            this.transform.position = new Vector3(desiredX, this.transform.position.y, this.transform.position.z);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UIElements;
+using NUnit.Framework.Internal;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -17,22 +18,26 @@ public class PlayerMovement : MonoBehaviour
     public AudioSource marioAudio;
     public AudioClip marioDeath;
     public float deathImpulse = 15;
-    
+    public Transform gameCamera;
+
+    [NonSerialized]
+    public bool alive = true;
+
     private Rigidbody2D _marioBody;
     private SpriteRenderer _marioSprite;
     private bool _faceRightState = true;
     private bool _onGroundState = true;
-    
+
     private void Start()
     {
         _marioSprite = GetComponent<SpriteRenderer>();
-        
+
         Application.targetFrameRate = 30;
         _marioBody = GetComponent<Rigidbody2D>();
 
         marioAnimator.SetBool("onGround", _onGroundState);
     }
-    
+
     private void Update()
     {
         if (Input.GetKeyDown("a") && _faceRightState)
@@ -43,12 +48,12 @@ public class PlayerMovement : MonoBehaviour
             if (_marioBody.linearVelocity.x > -0.1f)
                 marioAnimator.SetTrigger("onSkid");
         }
-        
+
         if (Input.GetKeyDown("d") && !_faceRightState)
         {
             _faceRightState = true;
             _marioSprite.flipX = false;
-            
+
             if (_marioBody.linearVelocity.x < 0.1f)
                 marioAnimator.SetTrigger("onSkid");
         }
@@ -58,27 +63,30 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-
-        if (Mathf.Abs(moveHorizontal) > 0)
+        if (alive)
         {
-            Vector2 movement = new Vector2(moveHorizontal, 0);
+            float moveHorizontal = Input.GetAxisRaw("Horizontal");
 
-            if (_marioBody.linearVelocity.magnitude < maxSpeed)
-                _marioBody.AddForce(movement * speed);
-        }
+            if (Mathf.Abs(moveHorizontal) > 0)
+            {
+                Vector2 movement = new Vector2(moveHorizontal, 0);
 
-        if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
-            _marioBody.linearVelocity = Vector2.zero;
+                if (_marioBody.linearVelocity.magnitude < maxSpeed)
+                    _marioBody.AddForce(movement * speed);
+            }
 
-        if (Input.GetKeyDown("space") && _onGroundState)
-        {
-            _marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
-            _onGroundState = false;
-            marioAnimator.SetBool("onGround", _onGroundState);
+            if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+                _marioBody.linearVelocity = Vector2.zero;
+
+            if (Input.GetKeyDown("space") && _onGroundState)
+            {
+                _marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+                _onGroundState = false;
+                marioAnimator.SetBool("onGround", _onGroundState);
+            }
         }
     }
-    
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -90,11 +98,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy") && alive)
         {
             Debug.Log("Collided with Goomba");
-            // Time.timeScale = 0.0f;
-            ResetGame();
+
+            StartCoroutine(DieAndResetSequence());
         }
     }
 
@@ -102,14 +110,14 @@ public class PlayerMovement : MonoBehaviour
     {
         // Reset position
         _marioBody.transform.position = new Vector3(-0.023f, -1.07f, 0f);
-        
+
         // Reset sprite direction
         _faceRightState = true;
         _marioSprite.flipX = false;
-        
+
         // Reset score
         scoreText.text = "Score: 0";
-        
+
         // Reset Goomba
         foreach (Transform eachChild in enemies.transform)
         {
@@ -117,10 +125,33 @@ public class PlayerMovement : MonoBehaviour
         }
 
         jumpOverGoomba.score = 0;
+
+        // Reset animation
+        marioAnimator.SetTrigger("gameRestart");
+        alive = true;
+
+        // Reset camera position
+        gameCamera.position = new Vector3(0, 0, -10);
+    }
+
+    private IEnumerator DieAndResetSequence()
+    {
+        marioAnimator.Play("mario-die");
+        marioAudio.PlayOneShot(marioDeath);
+        alive = false;
+
+        yield return new WaitForSeconds(5.0f);
+
+        ResetGame();
     }
 
     private void PlayJumpSound()
     {
         marioAudio.PlayOneShot(marioAudio.clip);
+    }
+
+    private void PlayDeathImpulse()
+    {
+        _marioBody.AddForce(Vector2.up * deathImpulse, ForceMode2D.Impulse);
     }
 }
